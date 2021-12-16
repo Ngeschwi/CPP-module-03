443dae159d1be629116253bb291478d7aba0d256
Nathan Geschwind <ngeschwi@z1r11p1.42lyon.fr>
2021-12-16 09:36:19 +0100

    fin

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -3,12 +3,10 @@
 int	main()
 {
 	ClapTrap	clap_A("Bob");
-	ClapTrap	clap_B("Jean");
 
 	clap_A.attack("Jean");
-	clap_B.takeDamage(6);
-	clap_B.takeDamage(6);
-	clap_B.beRepaired(10);
+	clap_A.takeDamage(6);
+	clap_A.beRepaired(10);
 
 	return 0;
 }
